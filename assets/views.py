b8a4ce0033af375044f899c32130913995916681
saylorsd <steven.saylor@pitt.edu>
2020-03-23 15:54:03 -0400

    added search to asset endpoint moved asset url definitions to own file

--- assets/views.py
+++ assets/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.pagination import LimitOffsetPagination
 
 from assets.models import Asset, AssetType, Category
@@ -9,6 +9,8 @@ from assets.serializers import AssetSerializer, AssetGeoJsonSerializer, AssetLis
 class AssetViewSet(viewsets.ModelViewSet):
     queryset = Asset.objects.all()
     pagination_class = LimitOffsetPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', ]
 
     def get_serializer_class(self, *args, **kwargs):
         fmt = self.request.GET.get('fmt', None)
