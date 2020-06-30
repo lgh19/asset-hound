7bcc984cdd956fb22a4f9a88f03d4de2a82b276a
David Walker <david.walker@pitt.edu>
2020-06-30 21:42:42 +0000

    Add checks and improved console ouput when deleting assets by ID range

--- assets/management/commands/delete_assets_by_id_range.py
+++ assets/management/commands/delete_assets_by_id_range.py
@@ -6,7 +6,6 @@ from django.core.management.base import BaseCommand
 
 from assets.models import Asset
 
-
 class Command(BaseCommand):
     help = 'Delete assets between the specified id values.\n\nUsage:\n> python manage.py delete_assets_by_id_range <id_lower> <id_upper>'
 
@@ -16,11 +15,18 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         if len(args) == 2:
-            assets_iterator = Asset.objects.filter(id__gte=args[0], id__lte=args[1])
-            if input(f"About to delete all assets bewteen id = {args[0]} and {args[1]}. \nAre you sure? (y/n) ") != "y":
+            assert int(args[0]) <= int(args[1]) # Check that the command-line arguments
+                                                # represent a valid range.
+            if int(args[1]) - int(args[0]) > 50000:
+                print(f"Preparing to deleting {int(args[1]) - int(args[0]):,} records!")
+            if int(args[0]) > 200776 or int(args[1]) > 200776:
+                raise ValueError(f"That range includes currently protected v1 assets.")
+
+            if input(f"About to delete all {int(args[1]) - int(args[0]):,} assets between id = {int(args[0]):,} and {int(args[1]):,}. \nAre you sure? (y/n) ") != "y":
                 print("OK, never mind.")
                 exit()
-            print(f"Deleting all assets between id = {args[0]} and {args[1]}...")
+            assets_iterator = Asset.objects.filter(id__gte=args[0], id__lte=args[1])
+            print(f"Deleting all {int(args[1]) - int(args[0]):,} assets between id = {args[0]} and {args[1]}...")
 
             assets_iterator.delete()
             print(f"Done.")
