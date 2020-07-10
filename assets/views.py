eda14fcaeb7f53fda464b209b1261345d9d9c0dc
drw <drw@users.noreply.github.com>
2020-07-10 10:04:21 -0400

    Update parcel_id and residence and handle Locations/Organizations directly

--- assets/views.py
+++ assets/views.py
@@ -13,25 +13,16 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from assets.forms import UploadFileForm
 
+def boolify(x):
+    if x.lower() in ['true', 't']:
+        return True
+    if x.lower() in ['false', 'f']:
+        return False
+    return None
+
 def list_of(named_things):
     # This converts ManyToManyField values back to a list.
     return [t.name for t in named_things.all()]
-
-def process_many_to_many_field(row, destination_asset, source_field_name, destination_field_name, more_results):
-    asset_types = row['asset_type'].split('|') # The tricky thing about generalizing this is that there is a source field name
-    list_of_old_types = list_of(destination_asset.asset_types) # and a table-ized field name, and these may differ.
-    #names_of_old_types = [t.name for t in destination_asset.asset_types]
-    if set(asset_types) != set(list_of_old_types):
-        more_results.append(f"asset_type {'will be ' if mode == 'validate' else ''}changed from {set(list_of_old_types)} to {set(asset_types)}.")
-        try:
-            validated_asset_types = [AssetType.objects.get(name=asset_type) for asset_type in asset_types] # Change get to get_or_create to allow creation of new asset types.
-        except assets.models.AssetType.DoesNotExist:
-            more_results.append("Unable to find one of these asset types: {asset_types}.\n ABORTING!!!")
-            break
-        finally:
-            destination_asset.asset_types.set(validated_asset_types)
-    return destination_asset, more_results
-
 
 def handle_uploaded_file(f, mode):
     import csv
@@ -61,6 +52,9 @@ def handle_uploaded_file(f, mode):
             destination_asset_iterator = Asset.objects.filter(id = asset_id)
             assert len(destination_asset_iterator) == 1 # To ensure it exists in the database.
             destination_asset = destination_asset_iterator[0]
+
+            location = destination_asset.location
+            organization = destination_asset.organization
 
             more_results = [f"Preparing to link raw assets with IDs {[r.id for r in raw_assets]} and names {[r.name for r in raw_assets]} to asset with PREVIOUS name {destination_asset.name}."]
 
@@ -94,37 +88,52 @@ def handle_uploaded_file(f, mode):
 
             source_field_name = 'street_address'
             new_value = row[source_field_name]
-            old_value = destination_asset.location.street_address
+            old_value = location.street_address
             if new_value != old_value:
                 more_results.append(f"{source_field_name} {'will be ' if mode == 'validate' else ''}changed from {old_value} to {new_value}.")
-                destination_asset.location.street_address = new_value
+                location.street_address = new_value
 
             source_field_name = 'city'
             new_value = row[source_field_name]
-            old_value = destination_asset.location.city
+            old_value = location.city
             if new_value != old_value:
                 more_results.append(f"{source_field_name} {'will be ' if mode == 'validate' else ''}changed from {old_value} to {new_value}.")
-                destination_asset.location.city = new_value
+                location.city = new_value
 
             source_field_name = 'state'
             new_value = row[source_field_name]
-            old_value = destination_asset.location.state
+            old_value = location.state
             if new_value != old_value:
                 more_results.append(f"{source_field_name} {'will be ' if mode == 'validate' else ''}changed from {old_value} to {new_value}.")
-                destination_asset.location.state = new_value
+                location.state = new_value
 
             source_field_name = 'zip_code'
             new_value = row[source_field_name]
-            old_value = destination_asset.location.zip_code
+            old_value = location.zip_code
             if new_value != old_value:
                 more_results.append(f"{source_field_name} {'will be ' if mode == 'validate' else ''}changed from {old_value} to {new_value}.")
-                destination_asset.location.zip_code = new_value
+                location.zip_code = new_value
+
+            source_field_name = 'parcel_id'
+            new_value = row[source_field_name]
+            old_value = location.parcel_id
+            if new_value != old_value:
+                more_results.append(f"{source_field_name} {'will be ' if mode == 'validate' else ''}changed from {old_value} to {new_value}.")
+                location.parcel_id = new_value
+
+            source_field_name = 'residence'
+            new_value = boolify(row[source_field_name])
+            old_value = location.residence
+            if new_value != old_value:
+                more_results.append(f"{source_field_name} {'will be ' if mode == 'validate' else ''}changed from {old_value} to {new_value}.")
+                location.residence = new_value
 
             if mode == 'update':
-                more_results.append("Remember to verify that the Location updates.")
+                more_results.append("Updating associated Asset, RawAsset, Location, and Organization instances. (This may leave some orphaned.)")
                 #destination_asset.save()
+                #location.save()
+                #organization.save()
                 #raw_assets.save()
-            more_results.append("Remember to verify that the Location updates.")
 
             results += more_results
 
