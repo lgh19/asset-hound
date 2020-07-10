205b113100de8e8dc0fa2cace0904165956cceae
drw <drw@users.noreply.github.com>
2020-07-10 10:27:39 -0400

    Generalize abstract updating to scalar fields by passing field type

--- assets/views.py
+++ assets/views.py
@@ -27,8 +27,8 @@ def list_of(named_things):
     # This converts ManyToManyField values back to a list.
     return [t.name for t in named_things.all()]
 
-def check_or_update_value(instance, source_field_name, row, mode, more_results):
-    new_value = row[source_field_name]
+def check_or_update_value(instance, row, mode, more_results, source_field_name, field_type=str):
+    new_value = field_type(row[source_field_name])
     old_value = getattr(instance, source_field_name)
     if new_value != old_value:
         more_results.append(f"{source_field_name} {'will be ' if mode == 'validate' else ''}changed from {old_value} to {new_value}.")
@@ -97,7 +97,7 @@ def handle_uploaded_file(f, mode):
                 validated_values = [Tag.objects.get_or_create(name=value)[0] for value in new_values]
                 destination_asset.tags.set(validated_values)
 
-            location, more_results = check_or_update_value(location, row, mode, more_results, source_field_name = 'street_address')
+            location, more_results = check_or_update_value(location, row, mode, more_results, source_field_name = 'street_address', field_type=str)
 
             source_field_name = 'city'
             new_value = row[source_field_name]
