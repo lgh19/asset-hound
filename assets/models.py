d76326c12675708b72c6c909b0425dc7a674e341
drw <drw@users.noreply.github.com>
2020-10-11 23:33:43 -0400

    Fix import of delete_from_carto_by_id

--- assets/models.py
+++ assets/models.py
@@ -4,7 +4,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 from simple_history.models import HistoricalRecords
 
 from assets.utils import geocode_address
-from assets.management.commands.sync_to_carto import delete_from_carto_by_id
+from assets.util_carto import delete_from_carto_by_id
 
 from pprint import pprint
 
